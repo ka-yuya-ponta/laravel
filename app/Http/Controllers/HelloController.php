3fb814c842cf35dd43f0ab72dd249c516a6dd65e
ka-yuya-ponta <83522000+ka-yuya-ponta@users.noreply.github.com>
2021-06-25 10:58:40 +0900

    second commit

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('hello.index', ['txt'=>'フォームを入力']);
+    }
+    public function post(Request $request)
+    {
+        $validate_rule=[
+           'name'=>'required',
+           'email'=>'email',
+           'age'=>'numericlbetween:0,150' 
+        ];
+        $this->validate($request,$validate_rule);
+        return view('hello.index',['txt'=>'正しい入力です']);
+    }
+
+
+}
